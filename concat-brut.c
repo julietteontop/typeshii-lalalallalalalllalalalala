3f8d255e5fddf974b8f9b3bf7bf04a29a5f94d07
Marion1521 <mariondaudet313@gmail.com>
2026-07-23 13:41:22 +0000

    concaténation brute

--- concat-brut.c
+++ concat-brut.c
@@ -16,4 +16,26 @@ typedef struct track {
  } track_t ;
 
 
+//concatène tous les sons du track pour en faire un unique sound --> concaténation brute
+sound_t* reduce_track(track_t* t){
+    sound_t* s=malloc(sizeof(sound_t));
+
+    s->n_samples=0;
+    for(int k=0; k<t->n_sounds; k++){
+        s->n_samples+=t->sounds[k]->n_samples;
+    }
+
+    s->samples=malloc(s->n_samples*sizeof(int16_t));
  
+    int indice = 0;
+    for (int i = 0; i < t->n_sounds; i++) {
+        sound_t* courant = t->sounds[i];
+        for (int j = 0; j < courant->n_samples; j++) {
+            s->samples[indice] = courant->samples[j];
+            indice++;
+        }
+    }
+
+    return s;
+
+}
